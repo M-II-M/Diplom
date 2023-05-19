790f74a0ee7ac9f4832459ba31a7df437fac3f87
FrellHelper <60715096+FrellHelper@users.noreply.github.com>
2023-05-19 11:01:18 +0500

    asd

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+# from flask import Flask
+import whisper
 
 
-if __name__ == '__main__':
-    app.run()
+# app = Flask(__name__)
+#
+#
+# @app.route('/')
+# def index():
+#     model = whisper.load_model("large")
+#     result = model.transcribe("test.ogg", language="ru")
+#     print(result["text"])
+#     return 'Diplom'
+#
+#
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+
+model = whisper.load_model("large")
+result = model.transcribe("test.ogg", language="ru")
+print(result["text"])
